049ca4d1d40e4bf9fa9a29893c81acb6007a50fc
Rashmi4555 <rashmibm3579@gmail.com>
2025-04-04 10:49:20 +0530

    Savethisfile

---
+++ methoverloading.java
@@ -0,0 +1,15 @@
+public class methoverloading {
+    static int method(int a,int b){
+        return a+b;
+    }
+    double method(double a,double b){
+        return a+b;
+    }
+    public static void main(String[]args){
+        int sum1 = method(5,4);
+        methoverloading obj= new methoverloading();
+        double sum = obj.method(5.4,2.4);
+        System.out.println("Sum is"+sum1);
+        System.out.println("Sum is"+sum);
+    }
+}
